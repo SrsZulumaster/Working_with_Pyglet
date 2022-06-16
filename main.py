b40cbbe52d2e683caa63ecd4aeaffab1697da0a4
SrsZulumaster <minaolenzimmer@gmail.com>
2022-06-16 15:08:02 +0300

    Working with Pyglet First Commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import pyglet
+from pyglet import *
+from pyglet.gl import glClearColor
+
+
+class MyWindow(pyglet.window.Window):
+    def __init__(self, width, height, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.set_size(width, height)
+        background_colour = [255, 255, 255, 0]
+        # background_colour = [i / 255 for i in background_colour]
+        glClearColor(*background_colour)
+
+    def on_draw(self):
+        window.clear()
+
+    def on_key_press(self, symbol, modifiers):
+        pass
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        pass
+
+    def on_mouse_release(self, x, y, button, modifiers):
+        pass
+
+    def update(self, *args):
+        pass
+
+    def on_resize(self, width, height):
+        pass
+        # self.set_size(width, height)
+
+
+framerate = 30
+window = MyWindow(700, 700)
+pyglet.clock.schedule_interval(window.update, 1 / framerate)
+pyglet.app.run()
